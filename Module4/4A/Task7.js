20876030a0b1b7ae367104c9a3c1b6ce633dd54f
ephyoma <fyomaokorie@gmailcom>
2025-11-20 00:07:39 +0100

    Javascript program if, else if and else

---
+++ Module4/4A/Task7.js
@@ -0,0 +1,31 @@
+// Step 1: Initialize the triangle sides
+const side1 = 5
+const side2 = 5
+const side3 = 5 // Equilateral
+
+
+/*
+const side1 = 5
+const side2 = 5
+const side3 = 8 // Isosceles
+*/
+
+
+/*
+const side1 = 3
+const side2 = 5
+const side3 = 7  // Scalene
+
+
+*/
+
+// Step 2: Determine the type of triangle
+if (side1 === side2 && side2 === side3) {
+    console.log("Equilateral triangle");
+} 
+else if (side1 === side2 || side1 === side3 || side2 === side3) {
+    console.log("Isosceles triangle");
+} 
+else {
+    console.log("Scalene triangle");
+}
