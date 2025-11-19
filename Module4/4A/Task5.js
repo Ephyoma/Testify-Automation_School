c5f97c1fa2ac3da3d024bd45b0949264f6ef5801
ephyoma <fyomaokorie@gmailcom>
2025-11-19 23:37:23 +0100

    Javascript program to compare old and even numbers

--- Module4/4A/Task5.js
+++ Module4/4A/Task5.js
@@ -3,3 +3,11 @@ const position = '3'
 const output = 'Today is '+day+'. '+'It is the '+position+'rd '+'day of the week'
 
 console.log(output)
+
+
+/*
+ANd -> && All musy be true
+OR --> || Either must be true
+NOT --> ! tougle Boolean alue
+
+*/
---
+++ Module4/4A/Task6.js
@@ -0,0 +1,12 @@
+// Step 1: Initialize a number
+
+const number = 25
+
+// Step 2: Check if the number is divisible by 2
+
+if (number % 2 === 0) {
+    console.log(number + " is even");
+} else {
+    console.log(number + " is odd");
+}
+
