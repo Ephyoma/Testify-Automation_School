90271682ef2c377bf94df002837181aa9cd09f9e
ephyoma <fyomaokorie@gmailcom>
2025-11-20 04:14:34 +0100

    Javascript program loop

---
+++ Module4/4A/loop.js
@@ -0,0 +1,11 @@
+let star = 1 //Starting point 
+while(star <=100) {
+    if (star ===1) {
+        console.log(star +' star')
+        
+    } else {
+            console.log(star +' stars')
+        }
+
+    star = star + 1
+}
---
+++ Module4/4A/practice.js
@@ -0,0 +1,36 @@
+const day = 'Saturday'
+
+/*if(day === 'Friday'){
+    console.log('TGIF!')
+}
+else if(day === 'Saturday'){
+    console.log('Yel! its a weekend')
+}
+
+else if(day === 'Sunday'){
+    console.log('Happy Sunday!!')
+}
+
+else{
+    console.log('Go to work!!')
+}*/
+
+
+switch(day){
+    case 'Friday':
+     console.log('TGIF!')
+     break
+
+     case 'Saturday':
+        console.log('Yel! its a weekend')
+        break
+
+        case 'Sunday':
+        console.log('Happy Sunday!!')
+
+        break
+
+        default: console.log('Go to work!!')
+
+
+}
