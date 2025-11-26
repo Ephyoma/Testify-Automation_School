3f2e219068b3bd4a248c59d279ab7f1ff50a38c0
ephyoma <fyomaokorie@gmailcom>
2025-11-26 22:59:34 +0100

    Add Function program to return value

---
+++ Module4/4A/Task10.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * Task: Area Of a Rectangle 2
+
+
+
+Write JavaScript function, calculateAreaOfRectangle. The function should:
+
+Takes two parameters, length and width. 
+
+Multiplies the length and width
+
+Returns the area of the rectangle
+ * 
+ */
+
+function calculateAreaOfRectangle(length, width) {
+    const area = length * width;
+    return area;
+}
+
+
+console.log(calculateAreaOfRectangle(10,7));
+
+
