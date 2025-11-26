efac3edf9b7af559350b08385941bded49f969bc
ephyoma <fyomaokorie@gmailcom>
2025-11-26 22:05:09 +0100

    Add Function program

---
+++ Module4/4A/Task9.js
@@ -0,0 +1,14 @@
+/*Write JavaScript function, calculateAreaOfRectangle. The function should
+Take two parameters, length and width. 
+Multiply the length and width
+Log the product to the console*/
+
+
+
+function calculateAreaOfRectangle(length, width) {
+    const area = length * width;
+    console.log(area);
+}
+
+
+calculateAreaOfRectangle(10, 7)
