18fd2d0b97f75eec8fe2c66c8f905e436c0e1151
ephyoma <fyomaokorie@gmailcom>
2025-12-03 08:23:39 +0100

    JavaScript object representing a book

---
+++ Module4/4A/Task13.js
@@ -0,0 +1,30 @@
+/**My Personal Library */
+/**Task
+
+Using object literal, write a JavaScript object, books.
+
+The book object should take the following properties
+
+title (string)
+
+description (string)
+
+numberOfPages (number)
+
+authour (string)
+
+reading (boolean) */
+/**Push your code to your Github Repository and Ensure your Jenkins job run */
+/** JavaScript object representing a book */
+
+
+const myPersonalLibrary = {
+    title:'JavaScript Object Basics',
+    description:'Learning JavaScript object properties and methods',
+    numberOfPages: 120,
+    author: 'Ifeoma Onyemize',
+    reading:'true'
+
+ }
+
+ console.log(myPersonalLibrary.author)
