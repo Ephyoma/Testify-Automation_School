866a2b02bdca495dbe01e0336e429689aeca42d6
ephyoma <fyomaokorie@gmailcom>
2025-12-04 04:08:02 +0100

    JavaScript object representing toggle reading status

---
+++ Module4/4A/Task14.js
@@ -0,0 +1,32 @@
+/*
+My Personal Library 2
+Add a toggleReadingStatus method to your books objects. 
+
+The method should update the value of the reading property of the book object.
+
+Remember to log the reading property to the console to confirm the current status.
+*/
+
+
+
+
+
+const myPersonalLibrary = {
+    title:'JavaScript Object Basics',
+    description:'Learning JavaScript object properties and methods',
+    numberOfPages: 120,
+    author: 'Ifeoma Onyemize',
+    reading:'true',
+    toggleReadingStatus: function(){
+        if(myPersonalLibrary.reading===false) {
+            myPersonalLibrary.reading = true
+        } else {
+            myPersonalLibrary.reading = false
+        }
+    }
+
+ }
+
+ myPersonalLibrary.toggleReadingStatus()
+
+ console.log(myPersonalLibrary.reading)
