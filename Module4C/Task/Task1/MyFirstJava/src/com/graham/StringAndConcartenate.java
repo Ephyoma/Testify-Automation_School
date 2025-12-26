0e9c1d46118cc7e7014c73347aec13afb9e12945
ephyoma <fyomaokorie@gmailcom>
2025-12-26 16:35:48 +0100

    Add Module4C Task3 string concatenation task

---
+++ Module4C/Task/Task1/MyFirstJava/src/com/graham/StringAndConcartenate.java
@@ -0,0 +1,19 @@
+package com.graham;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class StringAndConcartenate {
+    public static void main(String[] args) {
+        //String and Sping concat
+
+        String CarName = "Lexus";
+        String Model = " 2016 ";
+        String NewName = CarName + Model;
+
+        System.out.println(NewName);
+
+        // String and Primitive Concarte
+
+
+    }
+}
---
+++ Module4C/Task/Task3/src/com/task3/SringConcartenate.java
@@ -0,0 +1,29 @@
+package com.task3;
+
+public class SringConcartenate {
+    public static void main(String[] args) {
+
+        //Create a variable and store your age in it.
+        // concatenate your age with a string and print it out on the
+        // console using the two methods of conacatenation youwere taught
+//1. String and concat
+        String birthdayWish = "Wishing You A Happy ";
+        int age =  50;
+
+        String celebration = (birthdayWish + age + " Birthday Celebration");
+
+        System.out.println(celebration);
+
+        // string and primitive concat
+        String name = "Ifeoma's ";
+       boolean isTodayYourBirthday = true;
+       int HowOld = 50 ;
+       String Event = " Birthday";
+
+        //System.out.println(""+ IsTodayYourBirthday);
+
+        //.concat() Method
+        System.out.println(name.concat( HowOld +  Event));
+
+    }
+}
