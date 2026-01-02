f9a40a2cf72fe96a3b1e54faa6c20878ca5b5028
ephyoma <fyomaokorie@gmailcom>
2026-01-02 12:00:27 +0100

    Adding Constructor class

---
+++ Module4C/Tasks/src/com/tasks/Task13.java
@@ -0,0 +1,32 @@
+package com.tasks;
+
+public class Task13 {
+    //Creating Constructors Classes Training
+    //Create 3 different constructors of a class A.
+
+    String body;
+    int wings;
+    boolean power;
+
+    public Task13(String pbody) {
+        this.body = pbody;
+    }
+
+    public Task13(String pbody, int pwings) {
+        this.wings = wings;
+        this.body = pbody;
+    }
+
+    public Task13(String pbody, int pwings, boolean Ppower) {
+        this.power = Ppower;
+        this.body = pbody;
+        this.wings = pwings;
+    }
+
+
+}
+
+
+
+
+}
---
+++ Module4C/Tasks/src/com/tasks/Task13B.java
@@ -0,0 +1,17 @@
+package com.tasks;
+
+public class Task13B {
+    public static void main(String[] args) {
+
+        // Object using constructor with 1 parameter
+        Task13 Object1 = new Task13("Metal");
+
+        // Object using constructor with 2 parameters
+        Task13 Object2 = new Task13("Metal", 2);
+
+        // Object using constructor with 3 parameters
+        Task13 Object3 = new Task13("Metal", 2, true);
+    }
+}
+
+
