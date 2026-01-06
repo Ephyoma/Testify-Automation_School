6beee45256f0710929dbf2c46f99ca9643e554a5
ephyoma <fyomaokorie@gmailcom>
2026-01-06 02:41:53 +0100

    Adding Interface and its Implementation

---
+++ Module4C/Tasks/src/com/tasks/Task17BImplement.java
@@ -0,0 +1,23 @@
+package com.tasks;
+
+public class Task17BImplement implements Task17Blogin{
+    @Override
+    public void username() {
+
+    }
+
+    @Override
+    public void password() {
+
+    }
+
+    @Override
+    public void forgotpassword() {
+
+    }
+
+    @Override
+    public void signInButton() {
+
+    }
+}
---
+++ Module4C/Tasks/src/com/tasks/Task17Blogin.java
@@ -0,0 +1,18 @@
+package com.tasks;
+
+//All testers in your unit must test that the correct username is entered,
+// correct password that matches the username and
+// that Login to the homePage or dashboard is successful.
+// As the test Lead, create an interface that the testers should
+
+public interface  Task17Blogin {
+
+    public abstract void username();
+
+    public abstract void  password();
+
+    public abstract void forgotpassword();
+
+    public abstract void signInButton();
+
+}
