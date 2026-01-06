b1f9036dfcb30589833451b201001f79cb27700d
ephyoma <fyomaokorie@gmailcom>
2026-01-06 03:58:02 +0100

    Adding Error Handling

--- Module4C/Tasks/src/com/tasks/Tasks18.java
+++ Module4C/Tasks/src/com/tasks/Tasks18.java
@@ -1,4 +1,32 @@
 package com.tasks;
 
+//Write a code that asks the user for age and that accepts the Integer value.
+// Handle the error that is encountered when the users tries to enter a name instead of age
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Tasks18 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter Your Age");
+
+            int age = scanner.nextInt();
+            // If input is correct
+
+            System.out.println("Your age is " + age);
+        } catch (InputMismatchException Invalid){
+            // Handle wrong input (name instead of number)
+            System.out.println("Invalid input! Please enter a number for age.");
+        }
+        scanner.close();
+        }
+
+
+
 }
+
