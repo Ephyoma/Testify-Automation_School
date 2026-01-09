0ffee537655bb5406361097ce6f2e94ba40cc0b5
ephyoma <fyomaokorie@gmailcom>
2026-01-09 05:06:52 +0100

    Adding use of XPATH LOCATION STRATEGY

---
+++ Module4D/Tasks/src/test/java/org/example/Task5.java
@@ -0,0 +1,45 @@
+package org.example;
+//Navigate to https://www.saucedemo.com/
+//Login using username and password.
+//Click on the login button,
+//click on the "add to cart" button of any product (Add only one item)
+//click the cart icon on the top right.
+//Click on checkout.  USE ONLY XPATH LOCATION STRATEGY
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Task5 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "F:\\Testify\\CI-CD-Lesson\\Testify-Automation_School\\Module4D\\Tasks\\src\\chromedriver.exe");
+        //Launch Web Browser
+        ChromeOptions options = new ChromeOptions();
+        //prevents password prompts
+        options.addArguments("--incognito");
+        WebDriver driver = new ChromeDriver(options);
+        //Maximax web browser
+        driver.manage().window().maximize();
+        //Navigate the url
+        driver.get("https://www.saucedemo.com/");
+        //Wait time to allow browser load properly
+        Thread.sleep(5000);
+        //Navigate Login
+        //Enter Username
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+        //Enter password
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+        //Click on login
+        driver.findElement(By.xpath("//input[@id='login-button']")).click();
+        //Navigate to item backpack and click add to cart
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
+        //Navigate to shopping cart link
+        driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
+        Thread.sleep(5000);
+        //Checkout
+        driver.findElement(By.xpath("//button[@id='checkout']")).click();
+
+    }
+}
